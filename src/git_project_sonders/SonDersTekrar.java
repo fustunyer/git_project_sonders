53346fa54f9cd94dbb6559542b7e70536c8d823a
fatih ustunyer <fustunyer1@gmail.com>
2020-08-16 23:18:49 +0300

    feature commit

--- src/git_project_sonders/SonDersTekrar.java
+++ src/git_project_sonders/SonDersTekrar.java
@@ -3,9 +3,9 @@ package git_project_sonders;
 public class SonDersTekrar {
 
 	public static void main(String[] args) {
-	System.out.println("21:55 Master branch");
-	
-	
+	System.out.println("21:55-Master branch");
+	System.out.println("22:05-Feature branch");
+	System.out.println("22:17 feature barnch");
 	}
 
 }
